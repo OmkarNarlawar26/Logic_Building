a9af6509f52f34a5a6d636f62b1c01a0e782a040
Omkar Narlawar <omkarnarlawar26@gmail.com>
2025-11-18 01:07:50 +0530

    Q1 - Q5

---
+++ Assignments/Assignment_25/program5.c
@@ -0,0 +1,58 @@
+// Accept N number from user and display Summation of digits of each number.
+
+#include<stdio.h>
+#include<stdlib.h>
+
+void DigitsSum(int Arr[], int iLength)
+{
+    int iCnt = 0;
+    int iDigit = 0;
+    int iTemp = 0;
+    int iSum = 0;
+    
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        iTemp = Arr[iCnt];
+        iSum = 0;
+
+        while(iTemp != 0)
+        {
+            iDigit = iTemp % 10;
+            iSum = iSum + iDigit;
+            iTemp = iTemp / 10;
+        }
+
+        printf("%d\t", iSum);
+    }
+}
+
+int main()
+{
+    int iSize = 0, iCnt = 0, iRet = 0;
+    int *p = NULL;
+
+    printf("Enter the number of element :\n");
+    scanf("%d",&iSize);
+
+    p = (int *)malloc(iSize * sizeof(int));
+
+    if(p == NULL)
+    {
+        printf("Unable to allocate memory");
+        return -1;
+    }
+
+    printf("Enter %d elements : ",iSize);
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        printf("\nEnter element %d : ",iCnt + 1);
+        scanf("%d",&p[iCnt]);
+    }
+
+    DigitsSum(p, iSize);
+
+    free(p);
+
+    return 0;
+}
