02c5dc68f611b4cf17200e59a0ca35bcfebc1e50
Omkar Narlawar <omkarnarlawar26@gmail.com>
2025-11-23 00:31:32 +0530

    Q1 -Q5

---
+++ Assignments/Assignment_30/program5.java
@@ -0,0 +1,76 @@
+/* Accept number of rows and columns from user and display below pattern.
+ Input : iRow = 4     iCol = 4
+ OUTPUT :
+            1   2   3   4
+            1   *   *   4
+            1   *   *   4
+            1   2   3   4
+*/
+
+////////////////////////////////////////////////////////////////////////
+//
+//  Required Import File
+//
+////////////////////////////////////////////////////////////////////////
+
+import java.util.*;
+
+////////////////////////////////////////////////////////////////////////
+//
+//  Function Name   :   Display
+//  Description     :   used to print patterns
+//  Input           :   Integer
+//  Output          :   Pattern
+//  Author          :   Omkar Sachin Narlawar
+//  Date            :   22/11/2025
+//
+////////////////////////////////////////////////////////////////////////
+
+class Pattern
+{
+    public void Display(int iRow,int iCol)
+    {
+        int i = 0, j = 0;
+
+        for(i = 1; i <= iRow; i++)
+        {
+            for(j = 1; j <= iCol; j++)
+            {
+                if((i == 1) || (j == 1) || (j == iCol) | (i == iRow))
+                {
+                    System.out.print(j+"\t");
+                }
+                else
+                {
+                    System.out.print("*\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+}// End of Class
+
+////////////////////////////////////////////////////////////////////////
+//
+//  Entry point function : Main
+//
+////////////////////////////////////////////////////////////////////////
+
+class program5
+{
+    public static void main(String A[])
+    {
+        int iValue1 = 0, iValue2 = 0;
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter number of Rows : ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter number of Columns : ");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+        pobj.Display(iValue1,iValue2);
+    }
+}// End of Main
