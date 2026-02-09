ced7bfdb9db46415f7ada72374f12bd5a7aa808f
Omkar Narlawar <omkarnarlawar26@gmail.com>
2026-02-09 14:50:25 +0530

    Q1 - Q5

---
+++ Assignments/Assignment_69/program1.c
@@ -0,0 +1,29 @@
+// Write a recursive program which display below pattern.
+// Output : *   *   *   *   *
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    static int iCnt = 1;
+
+    if(iCnt <= iNo)
+    {
+        printf("*\t");
+        iCnt++;
+
+        Display(iNo);
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter Number : \n");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+}
---
+++ Assignments/Assignment_69/program3.c
@@ -0,0 +1,27 @@
+// Write a recursive program which display below pattern.
+// Output : 5   4   3   2   1
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    if(iNo >= 1)
+    {
+        printf("%d\t",iNo);
+        iNo--;
+
+        Display(iNo);
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter Number : \n");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+}
