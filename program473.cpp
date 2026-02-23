6f5f693bf5536e871d937ac185464d29969a896f
Omkar Narlawar <omkarnarlawar26@gmail.com>
2026-02-23 16:03:01 +0530

    MyPrograms

---
+++ program473.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+using namespace std;
+
+class ArrayX
+{
+    public:
+        int *Arr;
+        int iSize;
+
+        ArrayX(int no)
+        {
+            cout<<"Inside constructor\n";
+            iSize = no;
+            
+            Arr = new int[iSize];
+        }
+        
+        ~ArrayX()
+        {
+            cout<<"Inside destructor\n";
+            delete [] Arr;
+        }
+
+        void Accept()
+        {
+            int iCnt = 0;
+
+            cout<<"Enter the elements\n";
+
+            for(iCnt = 0; iCnt < iSize ; iCnt++)
+            {
+                cin>>Arr[iCnt];
+            }
+        }
+
+        void Display()
+        {
+            int iCnt = 0;
+
+            cout<<"Elements of the array are : \n";
+
+            for(iCnt = 0; iCnt < iSize; iCnt++)
+            {
+                cout<<Arr[iCnt]<<"\t";
+            }
+            cout<<"\n";
+        }
+};
+
+int main()
+{
+    int iValue = 0;
+
+    cout<<"Enter the number of elements : \n";
+    cin>>iValue;
+
+    // step 1 : Allocate the memory
+    ArrayX *aobj = new ArrayX(iValue);
+
+    // step 2 : use the memory
+    aobj->Accept();
+    aobj->Display();
+
+    // step 3 : deallocate the memory
+    delete aobj;
+
+    return 0;
+}
